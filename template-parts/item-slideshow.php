7d6581e35208d4aa683907943fa929285bc617f1
amit <amirudra007@gmail.com>
2017-04-06 21:32:25 +0530

    Adds Template part for slideshow

--- template-parts/item-slideshow.php
+++ template-parts/item-slideshow.php
@@ -1,8 +1,11 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<section class="item-slideshow-wrapper">
+		<ul class="bxslider item-slideshow">
+				<?php
+				// code to loop through and display images
+				?>
+				<li><?php // code to display image ?></li>
+				<?php
+				// close the loop
+				?>
+		</ul>
+</section>
