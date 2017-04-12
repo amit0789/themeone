ad2c7429d77006371a39cc5d2ebeedeec694280f
amit <amirudra007@gmail.com>
2017-04-12 08:57:51 +0530

    added slideshow

--- inc/handmedown-slideshow.php
+++ inc/handmedown-slideshow.php
@@ -1,8 +1,24 @@
-function _s_slideshow_scripts(){
- function _amit_theme_slideshow_scripts(){
-  	wp_register_script('bxslider', STYLESHEETPATH . 'js/bx-slider.js');
-  	wp_enqueue_script('bxslider');
-  }
+<?php
   
- add_action( 'wp_enqueue_scripts', '_s_slideshow_scripts' );
- add_action( 'wp_enqueue_scripts', '_amit_theme_slideshow_scripts' ); and css only on singular templates (single post, single custom post type, etc)
+ -function _amit_theme_slideshow_scripts(){
+ -	wp_register_script('bxslider', STYLESHEETPATH . 'js/bx-slider.js');
+ -	wp_enqueue_script('bxslider');
+ +/**
+ + * Enqueues bx-slider's styles and scripts
+ + */
+ +function _amit_theme_slideshow_scripts() {
+ +
+ +	// check if it is a single post of post type handmedown
+ +	if ( is_singular( 'handmedown' ) ) {
+ +
+ +		wp_register_script( 'jquery-bxslider', get_stylesheet_directory_uri() . 'js/jquery.bxslider.js', array( 'jquery' ), '' );
+ +
+ +		wp_register_script( 'saurabh-slideshow', get_stylesheet_directory_uri() . 'js/slideshow.js', array( 'jquery-bxslider' ), '', true );
+ +
+ +		wp_enqueue_script( 'saurabh-slideshow' );
+ +
+ +
+ +		wp_register_style( 'jquery-bxslider', get_stylesheet_directory_uri() . 'css/jquery.bxslider.css' );
+ +
+ +		wp_enqueue_style( 'jquery-bxslider' );
+ +	}
